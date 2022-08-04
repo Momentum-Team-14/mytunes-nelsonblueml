12cda5cced688df18225dffcf913ae5c368b2b58
nelsonblueml <109123605+nelsonblueml@users.noreply.github.com>
2022-08-03 23:33:34 -0400

    added error checking

--- main.js
+++ main.js
@@ -7,58 +7,96 @@ searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
   let searchBox = document.querySelector('#search-box');
   let urlEnd = searchBox.value.replaceAll(' ', '+');
-  let searchUrl = `${searchBaseUrl}${searchBox.value}&limit=20`;
-  console.log('search url', searchUrl);
-  getSearchResults(searchUrl);
+  if (searchBox.value !== '') {
+    let searchUrl = `${searchBaseUrl}${searchBox.value}&limit=20`;
+    console.log('search url', searchUrl);
+    getSearchResults(searchUrl);
+  } else {
+    alert('You must enter a search term.')
+  }
 })
 
+// function getSearchResults(url) {
+//   fetch(url, {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'text/javascript; charset=utf-8' }
+//     })
+//     // response is whatever the fetch returns
+//     .then(response => response.json()
+//       )
+//     // data is whatever the above code returns, in this case response.json()
+//     .then(data => {
+//       let songs = data.results;
+//       console.log('In second .then()');
+//       console.log(`data.results: ${data.results}`);
+//       showSearchResults(songs);
+//     })
+// }
+
+// Testing for fetch failure - result.ok is false
+// Should raise an alert
+getSearchResults('foobar');
+
 function getSearchResults(url) {
-  fetch(url, {
-    method: 'GET',
-    headers: { 'Content-Type': 'text/javascript; charset=utf-8' }
-    })
-    // response is whatever the fetch returns
-    .then(response => response.json())
-    // data is whatever the above code returns, in this case response.json()
-    .then(data => {
-      let songs = data.results;
-      // console.log(songs);
-      showSearchResults(songs);
-    })
+    fetch(url) 
+  // response is whatever fetch returns
+  .then((response) => {
+    if (response.ok) { 
+      return response.json();
+    }
+    return Promise.reject(response); 
+  })
+  // data is whatever the above code returns, in this case response.json()
+  .then(data => {
+    let songs = data.results;
+    console.log('In second .then()');
+    console.log(`data.results: ${data.results}`);
+    showSearchResults(songs);
+  })
+  .catch((error) => {
+    console.log('Something went wrong.', error); 
+    alert('ERROR! Something went wrong. Please retry your search.\nJust testing here.')
+  });
 }
 
+
+
+
+
 function showSearchResults(songArray) {
-  console.log(songArray);
-
-  deleteOldSongs();
-
-  for (let songRec of songArray) {
-    let songDiv = document.createElement('div');
-    songDiv.classList.add('songRec');
-    container.appendChild(songDiv);
-
-    let imgDiv = document.createElement('img')
-    imgDiv.classList.add('image');
-    imgDiv.src = songRec.artworkUrl100;
-    songDiv.appendChild(imgDiv);
-
-    let titleDiv = document.createElement('div')
-    titleDiv.classList.add('title');
-    titleDiv.innerText = songRec.trackName;
-    songDiv.appendChild(titleDiv);
-
-    let artistDiv = document.createElement('div');
-    artistDiv.classList.add('band');
-    artistDiv.innerText = `${songRec.artistName}`;
-    songDiv.appendChild(artistDiv);
-
-    let audioElement = document.querySelector('#musicPlayer');
-    let currentSong = document.querySelector('.currentSong');
-
-    songDiv.addEventListener('click', playAudio);
-    function playAudio() {
-      audioElement.src = songRec.previewUrl;
-      currentSong.innerText = `Currently playing: ${songRec.trackName} by ${songRec.artistName}.`
+  console.log(`songArray: ${songArray}`);
+  if (songArray.length === 0) {
+    alert('No results found.');
+  } else {
+    deleteOldSongs();
+    for (let songRec of songArray) {
+      let songDiv = document.createElement('div');
+      songDiv.classList.add('songRec');
+      container.appendChild(songDiv);
+  
+      let imgDiv = document.createElement('img')
+      imgDiv.classList.add('image');
+      imgDiv.src = songRec.artworkUrl100;
+      songDiv.appendChild(imgDiv);
+  
+      let titleDiv = document.createElement('div')
+      titleDiv.classList.add('title');
+      titleDiv.innerText = songRec.trackName;
+      songDiv.appendChild(titleDiv);
+  
+      let artistDiv = document.createElement('div');
+      artistDiv.classList.add('band');
+      artistDiv.innerText = `${songRec.artistName}`;
+      songDiv.appendChild(artistDiv);
+  
+      let audioElement = document.querySelector('#musicPlayer');
+      let currentSong = document.querySelector('.currentSong');
+  
+      songDiv.addEventListener('click', playAudio);
+      function playAudio() {
+        audioElement.src = songRec.previewUrl;
+        currentSong.innerText = `Currently playing: ${songRec.trackName} by ${songRec.artistName}.`
+      }
     }
   }
 }
@@ -68,4 +106,6 @@ function deleteOldSongs() {
   while (containerDiv.lastElementChild) {
     containerDiv.removeChild(containerDiv.lastElementChild);
   }
+  let audioElement = document.querySelector('#musicPlayer');
+  audioElement.src = '';
 }
